265ec131511ba5eb11b935806b4aabccd63af8b3
Sahar Mostafa <sahar.mostafa@gmail.com>
2015-10-18 16:40:18 -0700

    Initial Commit

---
+++ AdyLixTests/AdyLixTests.m
@@ -0,0 +1,39 @@
+//
+//  AdyLixTests.m
+//  AdyLixTests
+//
+//  Created by Sahar Mostafa on 10/18/15.
+//  Copyright © 2015 Sahar Mostafa. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AdyLixTests : XCTestCase
+
+@end
+
+@implementation AdyLixTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
